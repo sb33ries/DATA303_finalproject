2a781f7262e8103ea2e263bf1473d6ca5bd1caf8
sb33ries <124833574+sb33ries@users.noreply.github.com>
2024-05-11 08:43:15 -0400

    changes

--- light_dark.js
+++ light_dark.js
@@ -1,5 +1,10 @@
-function toggleMode() {
-    var body = document.body;
-    body.classList.toggle("dark-mode");
+function toggleDarkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  var sliderCircle = document.getElementById("slider-circle");
+  if (element.classList.contains("dark-mode")) {
+    sliderCircle.style.transform = "translateX(30px)";
+  } else {
+    sliderCircle.style.transform = "translateX(0)";
   }
-  
+}
