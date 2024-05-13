220eeece33b37dcb2d5632b0ca2f4a153cdd841d
sb33ries <124833574+sb33ries@users.noreply.github.com>
2024-05-12 21:00:00 -0400

    more button

---
+++ table_contents.js
@@ -0,0 +1,8 @@
+function toggleTOC() {
+    var toc = document.getElementById("toc");
+    if (toc.style.display === "none") {
+      toc.style.display = "block";
+    } else {
+      toc.style.display = "none";
+    }
+  }
